2dc4a6e003ac1b7aad8712cb5cd4faeb8890ffc4
Serge Kuharev <sergei.kuharev@stidia.com>
2015-01-03 13:04:19 +0200

    - Regular expressions tests now are passed.

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,31 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return /\d/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        return /([a-z])\1/i.test(str);
     },
 
     endsWithVowel : function(str) {
-
+        return /[aeiou]$/i.test(str);
     },
 
     captureThreeNumbers : function(str) {
+        var res = /[\d]{3}/.exec(str);
 
+        return (res && res[0]) ? res[0] : false;
     },
 
     matchesPattern : function(str) {
-
+        //XXX-XXX-XXXX format
+        return /^\d{3}-\d{3}-\d{4}$/.test(str);
     },
     isUSD : function(str) {
+        var pattern = /^\$(\d{1,3})(,\d{3})*(\.\d{2})?$/;
 
+        return pattern.test(str);
     }
   };
 });
