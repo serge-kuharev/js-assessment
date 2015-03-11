85516cf9e85c71fc3f9cb6253ee75b73e730ffe9
Serge Kuharev <sergei.kuharev@stidia.com>
2015-03-11 20:17:45 +0200

    - Numbers tests are now passed successfully.

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        return (num >> (bit-1)) & 1;
     },
 
     base10: function(str) {
-
+        return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+        return String('00000000' + num.toString(2)).slice(-8);
     },
 
     multiply: function(a, b) {
-
+        return parseFloat((a*b).toFixed(5));
     }
   };
 });
