ae481355ca204a4925b8ce85dfb11ad12d729b6f
Serge Kuharev <sergei.kuharev@stidia.com>
2015-03-12 12:08:53 +0200

    - Async tests are now passed successfully.

--- app/async.js
+++ app/async.js
@@ -3,11 +3,31 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+        return new window.Promise(function(resolve, reject) {
+            resolve(value);
+        });
     },
 
     manipulateRemoteData : function(url) {
+        return new window.Promise(function(resolve, reject) {
+            var req = new XMLHttpRequest();
 
+            req.open('GET', url);
+
+            req.onload = function() {
+                var response = JSON.parse(req.responseText).people || [];
+
+                var result = [];
+                for (var i = 0; i < response.length; i++) {
+                    result.push(response[i].name);
+                }
+                result.sort();
+
+                resolve(result);
+            };
+
+            req.send();
+        });
     }
   };
 });
