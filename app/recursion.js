aabdde11e8b8d5cd400a1c5a4474aa10a883e8d4
Serge Kuharev <sergei.kuharev@stidia.com>
2015-03-12 12:47:10 +0200

    - Permutation tests are now passed successfully.

--- app/recursion.js
+++ app/recursion.js
@@ -22,7 +22,27 @@ define(function() {
             },
 
         permute: function(arr) {
-
+            var result = [],
+                usedChars = [];
+            function main(){
+                var i, ch;
+                for (i = 0; i < arr.length; i++) {
+                    // Remove one char from input array.
+                    ch = arr.splice(i, 1)[0];
+                    usedChars.push(ch);
+                    // If nothing left in input, then no future permutations possible. Copy array to result.
+                    if (arr.length === 0) {
+                        result.push(usedChars.slice());
+                    }
+                    // Go recursive.
+                    main();
+                    // Add char we removed before to input.
+                    arr.splice(i, 0, ch);
+                    usedChars.pop();
+                }
+                return result;
+            }
+            return main();
         }
     };
 });
