f63bdb4e932d1ec0a11c25c487f4282f277c4e60
Serge Kuharev <sergei.kuharev@stidia.com>
2015-03-12 11:23:02 +0200

    - Counter tests are now passed successfully.

--- app/count.js
+++ app/count.js
@@ -3,6 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+        console.log(start);
+        var current = start+1;
+
+        var timer = setInterval(function() {
+            console.log(current);
+            current++;
+
+            if (current > end) {
+                clearInterval(timer);
+            }
+
+        }, 100);
+
+        return {
+            cancel: function() {
+                clearInterval(timer);
+            }
+        };
     }
   };
 });
